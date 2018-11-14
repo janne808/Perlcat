344c2a566ff4c555f4b099aab48a2b1f103e3cb4
Janne Heikkarainen <jheikkarainen@ruptly.tv>
2018-11-14 16:58:11 +0100

    Add legal part 2

--- perlcat.pl
+++ perlcat.pl
@@ -10,13 +10,13 @@
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Dubstep is distributed in the hope that it will be useful,
+#  Perlcat is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with Dubstep.  If not, see <http://www.gnu.org/licenses/>.
+#  along with Perlcat.  If not, see <http://www.gnu.org/licenses/>.
 use strict;
 use warnings;
 use IO::Socket;
