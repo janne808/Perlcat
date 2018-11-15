73c93713cd0f187fad053962a42d48f7303343a8
Janne Heikkarainen <jheikkarainen@ruptly.tv>
2018-11-15 22:26:21 +0100

    Fix error message

--- perlcat.pl
+++ perlcat.pl
@@ -1,22 +1,4 @@
 #!/usr/bin/perl
-#  (C) 2018 Janne Heikkarainen <janne808@radiofreerobotron.net>
-#
-#  All rights reserved.
-#
-#  This file is part of Perlcat, simple netcat clone written in Perl.
-#
-#  Perlcat is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Perlcat is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with Perlcat.  If not, see <http://www.gnu.org/licenses/>.
 use strict;
 use warnings;
 use IO::Socket;
@@ -44,7 +26,7 @@ my $verbose=0;
 # parse arguments...
 # print usage if no arguments
 if($#ARGV == -1){
-    argerr("ERROR: No arguments!");
+    argerr("No arguments!");
 }
 
 # search for help argument
